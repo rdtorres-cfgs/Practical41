83d20d232ddc11bd150c61d435784281b289148f
roberto <rtorres@cifpbmoll.eu>
2021-11-26 19:53:05 +0100

    Task 5

--- app/src/main/java/com/example/practical41/MainActivity.java
+++ app/src/main/java/com/example/practical41/MainActivity.java
@@ -1,96 +1,43 @@
 package com.example.practical41;
 
-import android.content.DialogInterface;
 import android.os.Bundle;
-
-import android.util.Log;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.EditText;
-import android.widget.Spinner;
-import android.widget.TextView;
 import android.widget.Toast;
-
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
 
-public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
-    private static final String TAG = MainActivity.class.getSimpleName();
-    private String mSpinnerLabel = "";
-
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // Cración spinner
-        Spinner spinner = (Spinner) findViewById(R.id.label_spinner);
-        if (spinner != null) {
-            spinner.setOnItemSelectedListener(this);
-        }
-
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.labels_array, android.R.layout.simple_spinner_item);
-
-        adapter.setDropDownViewResource
-                (android.R.layout.simple_spinner_dropdown_item);
-
-        if (spinner != null) {
-            spinner.setAdapter(adapter);
-        }
-
     }
 
-    public void showText(View view) {
-        EditText editText = (EditText) findViewById(R.id.editText_main);
-        if (editText != null) {
-            // Assign to showString both the entered string and mSpinnerLabel.
-            String showString = (editText.getText().toString() + " - " + mSpinnerLabel);
-            // Assign to phoneNumberResult the view for text_phonelabel to prepare to show it.
-            TextView phoneNumberResult = (TextView) findViewById(R.id.text_phonelabel);
-            // Show the showString in the phoneNumberResult.
-            if (phoneNumberResult != null) phoneNumberResult.setText(showString);
-        }
+    public void showDatePickerDialog(View v) {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(), getString(R.string.date_picker));
     }
 
-    public void onClickShowAlert(View view) {
-        // Build the alert dialog.
-        AlertDialog.Builder myAlertBuilder = new AlertDialog.Builder(MainActivity.this);
-        // Set the dialog title.
-        myAlertBuilder.setTitle(R.string.alert_title);
-        // Set the dialog message.
-        myAlertBuilder.setMessage(R.string.alert_message);
-        // Add the buttons.
-        myAlertBuilder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int which) {
-                // User clicked OK button.
-                Toast.makeText(getApplicationContext(), R.string.pressed_ok,
-                        Toast.LENGTH_SHORT).show();
-            }
-        });
-        myAlertBuilder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int which) {
-                // User clicked the CANCEL button.
-                Toast.makeText(getApplicationContext(), R.string.pressed_cancel,
-                        Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        // Create and show the AlertDialog.
-        myAlertBuilder.show();
-
-    }
-    @Override
-    public void onItemSelected(AdapterView<?> adapterView, View view, int pos, long id) {
-        mSpinnerLabel = adapterView.getItemAtPosition(pos).toString();
-        showText(view);
+    public void showTimePickerDialog(View view) {
+        DialogFragment newFragment = new TimePickerFragment();
+        newFragment.show(getSupportFragmentManager(), getString(R.string.time_picker));
     }
 
-    @Override
-    public void onNothingSelected(AdapterView<?> adapterView) {
-        Log.d(TAG, getString(R.string.nothing_entered));
+    public void processDatePickerResult(int year, int month, int day) {
+        String month_string = Integer.toString(month + 1);
+        String day_string = Integer.toString(day);
+        String year_string = Integer.toString(year);
+        String dateMessage = (month_string + "/" + day_string + "/" + year_string);
+        Toast.makeText(this, getString(R.string.date) + dateMessage, Toast.LENGTH_SHORT).show();
     }
+
+    public void processTimePickerResult(int hourOfDay, int minute) {
+        String hour_string = Integer.toString(hourOfDay);
+        String minute_string = Integer.toString(minute);
+        String timeMessage = (hour_string + ":" + minute_string);
+        Toast.makeText(this, getString(R.string.time) + timeMessage, Toast.LENGTH_SHORT).show();
+    }
+
 }
